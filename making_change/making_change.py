1b40a7e2178e251fa8777bde58d39473250d1e72
Fullmetal235 <jjbillupsbrown@gmail.com>
2020-01-23 18:20:19 -0600

    MVP for day 2

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python
 
 import sys
+old_denominations = [1, 5, 10, 25, 50]
+
 
 def making_change(amount, denominations):
-  pass 
+    ways = [denominations[0]] * (amount + 1)
+    steps = 0
+    del denominations[0]
+
+    for value in denominations:
+        for higher_amount in range(value, amount + 1):
+            remainder = higher_amount - value
+            ways[higher_amount] += ways[remainder]
+            steps += 1
+
+    print(steps)
+    return ways[amount]
+
+
+print(making_change(350, old_denominations))
 
 
 if __name__ == "__main__":
